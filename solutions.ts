048e791a2b0b6bff560075d6160cd2a2f3998cca
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-05-07 21:08:32 +0600

    Add getProperty function to solutions.ts for property access

--- solutions.ts
+++ solutions.ts
@@ -20,4 +20,8 @@ function checkType(value: StringOrNumber): "String" | "Number" {
   return "Number";
 }
 
+// Problem 4
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
 
