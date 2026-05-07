693c0edac132ce24caad36ea01ad3528ee3ca229
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2026-05-07 21:08:10 +0600

    Refactor solutions.ts to add checkType function and improve code organization

--- solutions.ts
+++ solutions.ts
@@ -1,11 +1,23 @@
+// S M Samiul Hasan 
+
 // Problem 1
 function filterEvenNumbers(numbers: number[]): number[] {
   return numbers.filter((n) => n % 2 === 0);
 }
 
-// S M Samiul Hasan 
-
 // Problem 2
 function reverseString(str: string): string {
   return str.split("").reverse().join("");
 }
+
+// Problem 3
+type StringOrNumber = string | number;
+ 
+function checkType(value: StringOrNumber): "String" | "Number" {
+  if (typeof value === "string") {
+    return "String";
+  }
+  return "Number";
+}
+
+
